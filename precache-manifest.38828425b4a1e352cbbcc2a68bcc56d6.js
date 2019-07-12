bf3aa21e2c5fb63e889b1afa502a526240a06308
levijr <levijr.santos@gmail.com>
2019-07-12 07:47:11 -0300

    Fix build

---
+++ precache-manifest.38828425b4a1e352cbbcc2a68bcc56d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3eca9c5aca0b5888246f3823f3787f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d83155d66d49b756cbd",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "88c4459cfbd640e6094f",
+    "url": "./static/css/main.3836b7df.chunk.css"
+  },
+  {
+    "revision": "0d83155d66d49b756cbd",
+    "url": "./static/js/2.c3aaf44b.chunk.js"
+  },
+  {
+    "revision": "88c4459cfbd640e6094f",
+    "url": "./static/js/main.3db4ba21.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
