bc035a226d4d627fda7095f8c07ef736bd4731c9
levijr <levijr.santos@gmail.com>
2019-07-12 07:18:56 -0300

    build

---
+++ build/precache-manifest.d0a309ab2db20842d26efde2c0913df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1980fcf68dc440a8d29862f14354164a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d83155d66d49b756cbd",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "cb4f9fe4bc729e2f3a7c",
+    "url": "/static/css/main.aa415f85.chunk.css"
+  },
+  {
+    "revision": "0d83155d66d49b756cbd",
+    "url": "/static/js/2.c3aaf44b.chunk.js"
+  },
+  {
+    "revision": "cb4f9fe4bc729e2f3a7c",
+    "url": "/static/js/main.52bf1575.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
